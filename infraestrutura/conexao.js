9a43ed35b6e924199c1a5c160cc35e970237b2a9
Paulo Ricardo Silva de Araújo <54222357+pkomander@users.noreply.github.com>
2021-02-03 19:27:10 -0300

    Update conexao.js

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -4,7 +4,7 @@ const conexao = mysql.createConnection({
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: 'araujo2025',
+    password: '',
     database: 'agenda-petshop'
 })
 
